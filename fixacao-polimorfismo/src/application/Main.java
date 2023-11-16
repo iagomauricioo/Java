f842f17035bc93b219b33d42b0e4f8fb3f163edc
Iago Mauricio dos Santos Silva <118476701+iagomauricioo@users.noreply.github.com>
2023-11-16 09:19:10 -0300

    Exercício de fixação polimorfismo

---
+++ fixacao-polimorfismo/src/application/Main.java
@@ -0,0 +1,59 @@
+package application;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        List<Product> list = new ArrayList<>();
+
+        System.out.print("Enter the number of products: ");
+        int n = sc.nextInt();
+
+        for ( int i = 1; i <= n; i++) {
+            System.out.printf("Product #%d data:%n", i);
+            System.out.println("Common, used or imported (c/u/i)?");
+            char ch = sc.next().charAt(0);
+
+            sc.nextLine();
+
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+
+            if (ch == 'i') {
+                System.out.print("Customs fee: ");
+                double customsFee = sc.nextDouble();
+
+                list.add(new ImportedProduct(name, price, customsFee));
+            }
+            else if (ch == 'u') {
+                System.out.print("Manufacture date (DD/MM/YYYY):");
+                Date manufactureDate = sdf.parse(sc.next());
+
+                list.add(new UsedProduct(name, price, manufactureDate));
+            }
+            else if (ch == 'c') {
+                list.add(new Product(name, price));
+            }
+
+
+
+        }
+        System.out.println("PRICE TAGS: ");
+        for (Product p: list) {
+            System.out.println(p.priceTag());
+        }
+    }
+}
