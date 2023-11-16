60d79fe68037457a8b5344b3b53adc6548a6f368
Iago Mauricio dos Santos Silva <118476701+iagomauricioo@users.noreply.github.com>
2023-11-16 09:15:41 -0300

    Área retângulo

---
+++ rectangle/src/Main.java
@@ -0,0 +1,28 @@
+package application;
+
+import entities.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Enter rectangle width and height:");
+
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle rectangle = new Rectangle();
+
+        rectangle.width = sc.nextDouble();
+        rectangle.height = sc.nextDouble();
+
+        System.out.printf("AREA = %.2f%n", rectangle.area());
+        System.out.printf("PERIMETER = %.2f%n", rectangle.perimeter());
+        System.out.printf("DIAGONAL = %.2f%n", rectangle.diagonal());
+
+
+    }
+}
