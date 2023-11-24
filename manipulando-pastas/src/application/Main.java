08b737fe447d31a7b104205e4aba22b443f4483e
Iago <118476701+iagomauricioo@users.noreply.github.com>
2023-11-24 09:07:52 -0300

    Manipulando pastas

---
+++ manipulando-pastas/src/application/Main.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+//"C:\\Users\\iagom\\Documents"
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a folder path: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        File[] folders = path.listFiles(File::isDirectory);
+
+        System.out.println("FOLDERS: ");
+
+        for (File folder : folders) {
+            System.out.println(folder);
+        }
+
+        System.out.println();
+        System.out.println("----------/----------");
+        System.out.println();
+
+        File[] files = path.listFiles(File::isFile);
+
+        System.out.println("FILES: ");
+
+        for (File file : files) {
+            System.out.println(file);
+        }
+
+        boolean success = new File(strPath + "\\subdir").mkdir();
+        System.out.println("Directory created successfully: " + success);
+
+
+        sc.close();
+    }
+}
